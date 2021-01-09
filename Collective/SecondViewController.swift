3ef9b102ad6c5b685fa7cbce942e77ed7c9f2cca
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2021-01-08 20:39:02 -0800

    TableView setup
    Co-Authored-By: PudgyElderGod <54594141+pudgyeldergod@users.noreply.github.com>

--- Collective/SecondViewController.swift
+++ Collective/SecondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var listTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
