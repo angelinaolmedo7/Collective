e463c53200d85edc04bc4231889a3d45d5407d1c
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2021-01-10 21:22:30 -0800

    Signin pushes user forward
    Co-Authored-By: PudgyElderGod <54594141+pudgyeldergod@users.noreply.github.com>

--- Collective/Controller/AuthViewController.swift
+++ Collective/Controller/AuthViewController.swift
@@ -12,12 +12,14 @@ import UIKit
 class AuthViewController: UIViewController {
     
     
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.statusLabel.text = ""
     }
     
     @IBAction func signInPressed(_ sender: Any) {
@@ -26,6 +28,8 @@ class AuthViewController: UIViewController {
     
     
     func signInUser() {
+        self.statusLabel.text = "Signing in..."
+        
         let urlPath = "http://www.collectiveapp.site/signin.php"
         
         var request = URLRequest(url: URL(string: urlPath)!)
@@ -58,7 +62,21 @@ class AuthViewController: UIViewController {
                 {
                     if let unwrappedData = data {
                         let returnedData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue) // Response from web server hosting the database
-                        print(returnedData as Any)
+                        if let returnedData = returnedData {
+                            DispatchQueue.main.async { // i think?
+                                if returnedData == "1" {
+                                    // log in
+                                    self.performSegue(withIdentifier: "signin", sender: nil)
+                                }
+                                else if returnedData == "0"{
+                                    // incorrect
+                                    self.statusLabel.text = "Username and password do not match. Please try again."
+                                }
+                                else {
+                                    self.statusLabel.text = "Something's gone wrong on our end. Please try again later."
+                                }
+                            }
+                        }
                     }
                 }
             }
