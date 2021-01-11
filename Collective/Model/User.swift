173b561360441f48b6fa1188766453ea63e2359d
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2021-01-11 00:48:53 -0800

    User parses correctly
    Co-Authored-By: PudgyElderGod <54594141+pudgyeldergod@users.noreply.github.com>

--- Collective/Model/User.swift
+++ Collective/Model/User.swift
@@ -57,37 +57,32 @@ class User: NSObject {
        }
        
    func parseJSON(_ data:Data) {
-       var jsonResult = NSObject() // change to object?
-       do{
-           jsonResult = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions.allowFragments) as! NSObject
+        var jsonResult = NSObject() // change to object?
+        do{
+            jsonResult = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions.allowFragments) as! NSObject
            
-       } catch let error as NSError {
-           print(error)
-           
-       }
+        } catch let error as NSError {
+            print(error)
+        }
        
-    
-        print(jsonResult)
         var jsonElement = NSDictionary()
-       
         jsonElement = jsonResult as! NSDictionary
            
         let user = User()
            
         //the following insures none of the JsonElement values are nil through optional binding
-        if let user_id = jsonElement["user_id"] as? Int,
-            let user_name = jsonElement["user_name"] as? String,
+        if let user_id = jsonElement["user_id"] as? NSString,
+            let user_name = jsonElement["user_name"] as? NSString,
 //                let uesr_date = jsonElement["user_date"] as? Date,
-            let user_level = jsonElement["user_level"] as? Int
+            let user_level = jsonElement["user_level"] as? NSString
         {
-               
-        user.user_id = user_id
-        user.user_name = user_name
-//            user.user_date = user_date
-        user.user_level = user_level
+            user.user_id = user_id.integerValue
+            user.user_name = user_name as String
+//    //            user.user_date = user_date
+            user.user_level = user_level.integerValue
                
         }
-    
-        print(user)
-   }
+        
+    print(user.user_name)
+    }
 }
